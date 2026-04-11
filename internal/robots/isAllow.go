d727223cd7d15461d88ae826e37f5022b437c5c6
Purinutt Amartayavis <ice.ice5335@gmail.com>
2026-04-12 06:20:54 +0700

    Add: Check for empty robots.txt

--- internal/robots/isAllow.go
+++ internal/robots/isAllow.go
@@ -3,5 +3,9 @@ package robots
 import "github.com/jimsmart/grobotstxt"
 
 func (r Robots) IsAllow(agent string, uri string) bool {
+	if r.Raw == nil || *r.Raw == "" {
+		return true
+	}
+
 	return grobotstxt.AgentAllowed(*r.Raw, agent, uri)
 }
