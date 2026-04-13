2b12f53e9fcd7a2970cb05e6e522d1dd2efc3832
Purinutt Amartayavis <ice.ice5335@gmail.com>
2026-04-13 08:35:31 +0700

    Refactor: Move rabbitMQ setup and database to main func

--- cmd/crawler/master/main.go
+++ cmd/crawler/master/main.go
@@ -4,18 +4,52 @@ import (
 	"log"
 	"time"
 
+	"github.com/puriice/golibs/pkg/db"
 	"github.com/puriice/golibs/pkg/env"
+	"github.com/puriice/golibs/pkg/messaging"
+	"github.com/purrice/prawler/internal/config"
 	"github.com/purrice/prawler/internal/heartbeat"
 	"github.com/purrice/prawler/internal/master"
 )
+
+func setupListener() (*messaging.RabbitMQ, *messaging.RabbitListener) {
+	cfg := config.GetConfig()
+	amqpURL := env.Get("amqp_url", "amqp://guest:guest@localhost/")
+
+	rabbitMQ, err := messaging.NewRabbitMQ(amqpURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	broker, err := rabbitMQ.NewBroker(cfg.ExchangeName.Master)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listenerConfig := messaging.NewRabbitListenerConfig(cfg.ExchangeName.Master, "prawler.master.*")
+	listener, err := broker.NewListenerWithConfig(listenerConfig)
+
+	return rabbitMQ, listener
+}
 
 func main() {
 	env.Init()
 
 	holter := heartbeat.NewHolter(5*time.Second, 5*time.Second, 2*time.Second)
-
 	holter.Run()
-	master.Run()
 
+	rabbit, listener := setupListener()
+	defer rabbit.Shutdown()
+
+	db, err := db.NewDatabase()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	master.Run(db, *listener)
 	log.Println("Shuting down")
 }
--- internal/master/run.go
+++ internal/master/run.go
@@ -7,47 +7,14 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/puriice/golibs/pkg/db"
-	"github.com/puriice/golibs/pkg/env"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/puriice/golibs/pkg/messaging"
-	"github.com/purrice/prawler/internal/config"
 	"github.com/purrice/prawler/internal/fetch"
 	"github.com/purrice/prawler/internal/repository"
 	"github.com/purrice/prawler/internal/robots"
 )
 
-func Run() {
-	cfg := config.GetConfig()
-	amqpURL := env.Get("amqp_url", "amqp://guest:guest@localhost/")
-
-	rabbitMQ, err := messaging.NewRabbitMQ(amqpURL)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rabbitMQ.Shutdown()
-
-	broker, err := rabbitMQ.NewBroker(cfg.ExchangeName.Master)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	listenerConfig := messaging.NewRabbitListenerConfig(cfg.ExchangeName.Master, "prawler.master.*")
-	listener, err := broker.NewListenerWithConfig(listenerConfig)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db, err := db.NewDatabase()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
+func Run(db *pgxpool.Pool, listener messaging.RabbitListener) {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
